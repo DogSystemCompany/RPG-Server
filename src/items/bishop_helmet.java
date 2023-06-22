722d58fa420e3fdf4faede7fa82e71a0947c5596
DogSystem <93628265+DogSystemCompany@users.noreply.github.com>
2023-06-22 11:07:42 -0400

    Events/Items first upload.

---
+++ src/items/bishop_helmet.java
@@ -0,0 +1,63 @@
+package items;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import thirtyvirus.uber.UberItem;
+import thirtyvirus.uber.helpers.UberAbility;
+import thirtyvirus.uber.helpers.UberCraftingRecipe;
+import thirtyvirus.uber.helpers.UberRarity;
+import thirtyvirus.uber.helpers.Utilities;
+
+import java.util.List;
+
+public class bishop_helmet extends UberItem {
+
+    public bishop_helmet(Material material, String name, UberRarity rarity, boolean stackable, boolean oneTimeUse, boolean hasActiveEffect, List<UberAbility> abilities, UberCraftingRecipe craftingRecipe) {
+        super(material, name, rarity, stackable, oneTimeUse, hasActiveEffect, abilities, craftingRecipe);
+    }
+    public void onItemStackCreate(ItemStack item) { }
+    public void getSpecificLorePrefix(List<String> lore, ItemStack item) { }
+    public void getSpecificLoreSuffix(List<String> lore, ItemStack item) { }
+    public boolean rightClickAirAction(Player player, ItemStack item) { return false; }
+    public boolean leftClickBlockAction(Player player, PlayerInteractEvent event, Block block, ItemStack item) { return leftClickAirAction(player, item); }
+    public boolean leftClickAirAction(Player player, ItemStack item) {
+        if (Utilities.getIntFromItem(item, "ability1") == 0) {
+            Utilities.storeIntInItem(item, 1, "ability1");
+            Utilities.addEnchantGlint(item);
+            player.addPotionEffect(PotionEffectType.INCREASE_DAMAGE.createEffect(Integer.MAX_VALUE, 2));
+        } else {
+            Utilities.storeIntInItem(item, 0, "ability1");
+            ItemMeta meta = item.getItemMeta();
+            for (Enchantment e : meta.getEnchants().keySet()) {
+                meta.removeEnchant(e);
+            }
+            item.setItemMeta(meta);
+                for (PotionEffect effect : player.getActivePotionEffects()) { player.removePotionEffect(effect.getType()); }
+        }
+
+
+        return true;
+    }
+    public boolean rightClickBlockAction(Player player, PlayerInteractEvent event, Block block, ItemStack item) { return false; }
+    public boolean shiftLeftClickAirAction(Player player, ItemStack item) { return false; }
+    public boolean shiftLeftClickBlockAction(Player player, PlayerInteractEvent event, Block block, ItemStack item) { return false; }
+    public boolean shiftRightClickAirAction(Player player, ItemStack item) { return false; }
+    public boolean shiftRightClickBlockAction(Player player, PlayerInteractEvent event, Block block, ItemStack item) { return false; }
+
+    public boolean middleClickAction(Player player, ItemStack item) { return false; }
+    public boolean hitEntityAction(Player player, EntityDamageByEntityEvent event, Entity target, ItemStack item) { return false; }
+    public boolean breakBlockAction(Player player, BlockBreakEvent event, Block block, ItemStack item) { return false; }
+    public boolean clickedInInventoryAction(Player player, InventoryClickEvent event, ItemStack item, ItemStack addition) { return false; }
+    public boolean activeEffect(Player player, ItemStack item) { return false; }
+}
